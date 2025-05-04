dbe2796b90264ac53473679cf6d15b85a6e272f0
amirmonfaredse <monfared.dev@gmail.com>
2025-05-04 17:33:36 +0330

    Basic Structure

---
+++ app/(main)/layout.tsx
@@ -0,0 +1,17 @@
+import "../../public/styles/globals.css";
+
+import React, { ReactNode } from "react";
+
+type Props = {
+  children: ReactNode;
+};
+
+export default function RootLayout({ children }: Props) {
+  return (
+    <html lang="fa" dir="rtl">
+      <body>
+        <div>{children}</div>
+      </body>
+    </html>
+  );
+}
